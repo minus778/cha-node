1ad2c5fdedf576f88498f4d4311a9ee69adc63f3
minusing <1776875119@qq.com>
2022-06-20 19:40:16 +0800

    测试自动构建

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 global.secretJwt = '123456'
 
 app.get('/', (req, resp) => {
-  resp.send('hello')
+  resp.send('自动构建测试')
 })
 
 app.use('/api/home', homeApi)
